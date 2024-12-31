c4e1a05ff9893113ff83435a92788e482073e646
Ryan O. mackey <ryan.o.mackey@gmail.com>
2024-12-30 19:40:28 -0600

    init

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,8 @@
+import { useSearchParams } from "react-router";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
+import alphabet from "data/alphabet.json";
+import pantoneColors from "data/pantone-colors.json";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,10 +11,60 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export function loader({ context }: Route.LoaderArgs) {
-  return { message: context.VALUE_FROM_CLOUDFLARE };
+export async function clientLoader({ params }: Route.ClientLoaderArgs) {
+  return {
+    alphabet,
+    pantoneColors,
+  };
+}
+
+function toPascalCase(text: string) {
+  return text.replace(/(^\w|-\w)/g, clearAndUpper);
+}
+
+function clearAndUpper(text: string) {
+  return text.replace(/-/, " ").toUpperCase();
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+  const [searchParams] = useSearchParams();
+  const { alphabet, pantoneColors } = loaderData;
+  const { names, values } = pantoneColors;
+
+  const colors = names.reduce((acc, curr, index) => {
+    return {
+      ...acc,
+      [curr]: values[index],
+    };
+  }, {});
+
+  return (
+    <div className="mx-8">
+      <ul className="flex flex-wrap gap-4 justify-center p-4">
+        {alphabet.map((letter) => (
+          <li key={letter}>
+            <a href={`?letter=${letter}`}>{letter}</a>
+          </li>
+        ))}
+      </ul>
+      <ul className="flex flex-wrap gap-4 justify-center my-2">
+        {names
+          .filter((name) =>
+            name.startsWith(searchParams.get("letter")?.toLowerCase() || "a")
+          )
+          .map((name) => (
+            <li key={name} className="rounded border p-2 shadow">
+              <div
+                className="w-32 h-32"
+                style={{ backgroundColor: colors[name as keyof typeof colors] }}
+              />
+              <div className="max-w-32 h-12 text-wrap font-bold">
+                {toPascalCase(name)}
+              </div>
+              <div>{colors[name as keyof typeof colors]}</div>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 }
